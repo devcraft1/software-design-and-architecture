93ba3dd0968596af98cd0c03779c2ba3eb64244d
devcraft1 <ukonudennis64@gmail.com>
2021-04-17 07:05:32 +0100

    queue(FIFO)

---
+++ spreadOperator.js
@@ -0,0 +1,9 @@
+// string
+const name = "Zelda";
+const letters = [...name];
+console.log(letters);
+
+// numbers
+const amount = [100000, 10, 20];
+const numbers = [...amount];
+console.log(numbers);
