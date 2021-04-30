9d22cf703abda82413686723ce934a92f0d2450a
devcraft1 <ukonudennis64@gmail.com>
2021-04-30 22:01:12 +0100

    console

--- snippet/console.js
+++ snippet/console.js
@@ -6,8 +6,7 @@ console.info(console.info())
 console.debug(console.debug())
 
 
-// using console.table 
-
+//  console.table 
 function Name (first, last){
  this.first =first
  this.last=last
@@ -18,3 +17,28 @@ const jonah= new Name('jonah', 'samuel')
 const joseph = new Name('joseph', 'seth')
 
 console.table([john,jonah,joseph])
+
+// console.clear
+console.clear()
+
+
+// console.assert
+const errMessage = 'this is not the required number'
+for (let number =0; number <=4; number ++ ){
+    console.log('The number is ' + number)
+    console.assert(number % 2 ==0, {number, errMessage})
+}
+
+// console.dir
+const user ={
+    name: 'string',
+    class: 10
+}
+console.dir(user)
+
+
+// console.count
+for(let count =0; count > 4; count ++){
+  let result= console.count()
+  console.log(result)
+}
