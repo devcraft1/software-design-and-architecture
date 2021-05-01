e8449096b1e0131bd229b0ea1bce516444a11df9
devcraft1 <ukonudennis64@gmail.com>
2021-05-01 22:38:16 +0100

    includes

---
+++ snippet/includes.js
@@ -0,0 +1,16 @@
+
+
+const arr = [1,2,3,4,5,6]
+const array =['string','number','boolean','null']
+
+if(arr.includes(1)){
+    console.log('is included')
+}
+
+if(array.includes('strin')){
+    console.log('is included')  
+} else if(!array.includes('strin')){
+        console.log('strin is not included in array')
+    }
+
+
