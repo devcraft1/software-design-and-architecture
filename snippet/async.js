d7497a5dea19e2f877268caf6d2d1913c1bf0e5e
devcraft1 <ukonudennis64@gmail.com>
2021-05-02 19:58:32 +0100

    apiUrlencoded

--- snippet/async.js
+++ snippet/async.js
@@ -1,18 +1,17 @@
 function hello(name) {
-    return new Promise((resolve,reject) => {
-      setTimeout(() => {
-        resolve(`I am ${name}`);
-      }, 2000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`I am ${name}`);
+    }, 2000);
+  });
+}
+async function async_msg() {
+  try {
+    const name = "unknown";
+    let msg = await hello(name);
+    console.log(msg);
+  } catch (e) {
+    console.log("Error!", e);
   }
-  async function async_msg() {
-    try {
-        const name = 'unknown'
-      let msg = await hello(name);
-      console.log(msg);
-    }
-    catch(e) {
-      console.log('Error!', e);
-    }
-  }
-  async_msg(); //output - I am unknown.
+}
+async_msg(); //output - I am unknown.
