ed410e367e50c9c8473a69ec3e48f1191d0af5dd
devcraft1 <ukonudennis64@gmail.com>
2021-05-02 20:03:26 +0100

    folder-cleanUp

--- snippet/includes.js
+++ snippet/includes.js
@@ -1,14 +1,11 @@
-
-const arr = [1,2,3,4,5,6]
-if(arr.includes(1)){
-    console.log('is included')
+const arr = [1, 2, 3, 4, 5, 6];
+if (arr.includes(1)) {
+  console.log("is included");
 }
 
-const array =['string','number','boolean','null', 'object']
-if(array.includes('strin')){
-    console.log('is included')  
-} else if(!array.includes('strin')){
-        console.log('strin can not be found in array')
-    }
-
-
+const array = ["string", "number", "boolean", "null", "object"];
+if (array.includes("strin")) {
+  console.log("is included");
+} else if (!array.includes("strin")) {
+  console.log("strin can not be found in array");
+}
