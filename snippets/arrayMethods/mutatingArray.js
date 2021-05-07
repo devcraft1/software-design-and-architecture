56b7ee444f80a4d7e5976ed734eb8784896c7bca
devcraft1 <ukonudennis64@gmail.com>
2021-05-07 23:50:28 +0100

    array-methods

---
+++ snippets/arrayMethods/mutatingArray.js
@@ -0,0 +1,13 @@
+/* Mutating 
+These are array methods that do not change the original array rather return a new array.
+They include:
+1. push
+2. pop
+3. shit
+4. unshift
+*/
+
+
+
+
+
--- snippets/arrayMethods/nonMutatingArray.js
+++ snippets/arrayMethods/nonMutatingArray.js
@@ -1,4 +1,11 @@
+/* Non-mutating Arrays have the ability to modify the original and return a new array
+They include:
+1. ForEach
+2. Map
+3. 
+*/
 
+// FOR EACH
 // forEach with 2 arguments
 const avengers = ['thor', 'captain america', 'hulk'];
 avengers.forEach((item, index)=>{
@@ -10,4 +17,3 @@ let colors = ['red', 'blue', 'green'];
 colors.forEach(function(color, index, sourceArr) {
 	console.log(index, color, sourceArr,)
 });
-
