c81c303c489c1fb4c834e9273f7ce62716d0e48d
devcraft1 <ukonudennis64@gmail.com>
2021-05-07 23:27:18 +0100

    ternary-operator

--- snippets/ternaryOperator.js
+++ snippets/ternaryOperator.js
@@ -5,7 +5,7 @@ const result2 = condition == 1 ? true : false;
 console.log(` Result1 is ${result1} while result2 ${result2}`);
 
 // Beer or juice
-let age = 18;
+let age = 20;
 let drink =
   age !== 18 ? "authorized to take alcohol" : "not authorised to take alcohol";
 console.log(drink);
