7e2306f3a4e33d2abe20f30ef473e07a7fe19ccf
devcraft1 <ukonudennis64@gmail.com>
2021-05-07 23:15:38 +0100

    forEach

--- snippets/forEach.js
+++ snippets/forEach.js
@@ -1,8 +1,13 @@
 
-// forEach with index 
+// forEach with 2 arguments
 const avengers = ['thor', 'captain america', 'hulk'];
 avengers.forEach((item, index)=>{
 	console.log(index, item)
 })
 
+// forEach with 3 arguments 
+let colors = ['red', 'blue', 'green'];
+colors.forEach(function(color, index, sourceArr) {
+	console.log(index, color, sourceArr,)
+});
 
