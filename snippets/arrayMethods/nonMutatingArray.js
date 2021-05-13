1ff6d5b82bde2ae7ce526e06231adc0abcb5f58e
devcraft1 <ukonudennis64@gmail.com>
2021-05-13 23:21:12 +0100

    array-methods

--- snippets/arrayMethods/nonMutatingArray.js
+++ snippets/arrayMethods/nonMutatingArray.js
@@ -92,3 +92,21 @@ console.log(array1.reduce(reducer, 5));
 // LASTINDEXOF
 
 // SLICE
+const str = "The quick brown fox jumps over the lazy dog.";
+
+console.log(str.slice(31));
+// expected output: "the lazy dog."
+
+console.log(str.slice(4, 19));
+// expected output: "quick brown fox"
+
+console.log(str.slice(-4));
+// expected output: "dog."
+
+console.log(str.slice(-9, -5));
+// expected output: "lazy"
+
+console.log(str.slice(0, 2));
+// expected output: "the"
+// Up to and including the last index!!!
+// Different for python.
