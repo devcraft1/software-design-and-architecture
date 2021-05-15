28f5b038c18a306da2b6403d845b43419d4f9e39
devcraft1 <ukonudennis64@gmail.com>
2021-05-15 23:35:55 +0100

    array-methods

--- snippets/arrayMethods/mutatingArray.js
+++ snippets/arrayMethods/mutatingArray.js
@@ -11,21 +11,34 @@ They include:
 */
 
 // PUSH
+let animals = ["dog", "cat", "tiger"];
+console.log(animals.push("elephant"));
 
 // POP
-
-// SHIFT
+console.log(animals.pop());
 
 // UNSHIFT
+console.log(animals.unshift("cow"));
+
+// SHIFT
+console.log(animals.shift());
 
 // SORT
+var names = ["Peter", "Emma", "Jack", "Mia", "Eric"];
+names.sort(); // ["Emma", "Eric", "Jack", "Mia", "Peter"]
+
+var objs = [
+  { name: "Peter", age: 35 },
+  { name: "Emma", age: 21 },
+  { name: "Jack", age: 53 },
+];
+
+objs.sort(function (a, b) {
+  return a.age - b.age;
+}); // Sort by age (lowest first)
 
 // REVERSE
+const list = [1, 2, 3, 4, 5];
+list.reverse();
 
 // SPLICE
-
-
-
-
-
-
