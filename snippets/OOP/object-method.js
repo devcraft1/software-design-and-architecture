a0c24564a4f8ad6fcd5b4b7516a8f9d7f3eeffe5
devcraft1 <ukonudennis64@gmail.com>
2021-05-16 23:56:54 +0100

    object-oriented-programming

---
+++ snippets/OOP/object-method.js
@@ -0,0 +1,10 @@
+const testScore = {
+  damon: 89,
+  shawn: 91,
+  keenan: 80,
+  kim: 89,
+};
+
+Object.keys(testScore); // gives all keys
+Object.values(testScore); // gives all values
+Object.entries(testScore); // gives nested arrays of key-value pairs
