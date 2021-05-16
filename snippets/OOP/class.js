f244389af6d210b8371f54e16ceaded48585815f
devcraft1 <ukonudennis64@gmail.com>
2021-05-16 23:55:20 +0100

    object-oriented-programming

--- snippets/OOP/class.js
+++ snippets/OOP/class.js
@@ -14,3 +14,14 @@ const helloInstance = new Hello("john doe");
 console.log(helloInstance);
 
 // Object method
+/// OBJECTS IN JAVASCRIPT
+const testScore = {
+  damon: 89,
+  shawn: 91,
+  keenan: 80,
+  kim: 89,
+};
+
+Object.keys(testScore); // gives all keys
+Object.values(testScore); // gives all values
+Object.entries(testScore); // gives nested arrays of key-value pairs
---
+++ snippets/OOP/constructor.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const person = new Person("John Doe", 23);
+console.log(person);
