a4f2a5206bcfffe01520bd5f1ee0ab424ce97539
devcraft1 <ukonudennis64@gmail.com>
2021-05-19 20:28:58 +0100

    loop

--- snippets/loop.js
+++ snippets/loop.js
@@ -1,5 +1,10 @@
 // for loop
 
+// for - loops through a block of code a number of times.
+// for/in - loops through the properties of an object.
+// for/of - loops through the values of an iterable object.
+// while - loops through a block of code while a specified condition is true.
+
 var colors = ["red", "blue", "green"];
 for (let i = 0; i < colors.length; i++) {
   console.log(colors[i]);
