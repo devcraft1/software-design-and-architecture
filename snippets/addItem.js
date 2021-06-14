aa253effedde30997acaf70afef565f22408da9d
devcraft1 <ukonudennis64@gmail.com>
2021-06-14 23:50:21 +0100

    object-added

--- snippets/addItem.js
+++ snippets/addItem.js
@@ -1,10 +1,10 @@
-// Add an array
-const itemArray = [];
-const addItem = itemArray.push("fruits", "breverages");
-console.log(addItem);
-console.log(itemArray);
+// // Add an array
+// const itemArray = [];
+// const addItem = itemArray.push("fruits", "breverages");
+// console.log(addItem);
+// console.log(itemArray);
 
-// Add an Object
-const obj = {};
-const addObj = Object.assign(obj, { car: "Ferrari" });
-console.log(obj);
+// // Add an Object
+// const obj = {};
+// const addObj = Object.assign(obj, { car: "Ferrari" });
+// console.log(obj);
