e45bde6527f5d465d0afc1090bab1cd2d7c60f81
devcraft1 <ukonudennis64@gmail.com>
2021-07-06 23:18:16 +0100

    definition-of-value-pairs

--- snippets/OOP/key-value-pairs.js
+++ snippets/OOP/key-value-pairs.js
@@ -1,3 +1,5 @@
+const x =
+  "A key-value pair is two linked data items: a key, which is a unique identifier for some item of data, and the value, which is either the data that is identified or a pointer to the location of that data.";
 const testScore = {
   damon: 89,
   shawn: 91,
