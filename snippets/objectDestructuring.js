dabee8771299c4373a6c3db4dcf30a7e10462204
devcraft1 <ukonudennis64@gmail.com>
2021-07-23 21:04:36 +0100

    obj

--- snippets/objectDestructuring.js
+++ snippets/objectDestructuring.js
@@ -1,13 +1,13 @@
-//simple example with object------------------------------
-let obj = { names: "Max", age: 22, address: "Delhi" };
+// //simple example with object------------------------------
+// let obj = { names: "Max", age: 22, address: "Delhi" };
 
-const { names, age } = obj;
+// const { names, age } = obj;
 
-console.log(names);
-//expected output: "Max"
+// console.log(names);
+// //expected output: "Max"
 
-console.log(age);
-//expected output: 22
+// console.log(age);
+// //expected output: 22
 
-console.log(address);
-//expected output: Uncaught ReferenceError: address is not defined
+// console.log(address);
+// //expected output: Uncaught ReferenceError: address is not defined
