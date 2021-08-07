efdce99e654120e9b1be97d6347a7a88a86f636e
devcraft1 <ukonudennis64@gmail.com>
2021-08-07 20:02:00 +0100

    operators

--- snippets/ternaryOperator.js
+++ snippets/ternaryOperator.js
@@ -1,11 +1,11 @@
-// condition ? ifTrue : ifFalse
-const condition = 12 + 3;
-const result1 = condition == 15 ? true : false;
-const result2 = condition == 1 ? true : false;
-console.log(` Result1 is ${result1} while result2 ${result2}`);
+// // condition ? ifTrue : ifFalse
+// const condition = 12 + 3;
+// const result1 = condition == 15 ? true : false;
+// const result2 = condition == 1 ? true : false;
+// console.log(` Result1 is ${result1} while result2 ${result2}`);
 
-// Beer or juice
-let age = 20;
-let drink =
-  age !== 18 ? "authorized to take alcohol" : "not authorised to take alcohol";
-console.log(drink);
+// // Beer or juice
+// let age = 20;
+// let drink =
+//   age !== 18 ? "authorized to take alcohol" : "not authorised to take alcohol";
+// console.log(drink);
