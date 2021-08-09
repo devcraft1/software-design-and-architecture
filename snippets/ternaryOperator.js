013563e7d9f6e6644f11e531b21bd71e105c7c13
devcraft1 <ukonudennis64@gmail.com>
2021-08-09 22:31:49 +0100

    fetch

--- snippets/ternaryOperator.js
+++ snippets/ternaryOperator.js
@@ -4,7 +4,6 @@ const result1 = condition == 15 ? true : false;
 const result2 = condition == 1 ? true : false;
 console.log(` Result1 is ${result1} while result2 ${result2}`);
 
-// Beer or juice
 let age = 20;
 let drink =
   age !== 18 ? "authorized to take alcohol" : "not authorised to take alcohol";
