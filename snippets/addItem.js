1478cbe6631a5d65a5de98b213372e55380b36d1
devcraft1 <ukonudennis64@gmail.com>
2021-08-09 22:33:17 +0100

    fetch

--- snippets/addItem.js
+++ snippets/addItem.js
@@ -4,6 +4,7 @@ const addItem = itemArray.push("fruits", "breverages");
 console.log(addItem);
 console.log(itemArray);
 
+// Add an Object
 const obj = {};
 const addObj = Object.assign(obj, { car: "Ferrari" });
 console.log(obj);
