fd6e7c6d817c2562dfd84fc23633e2e33426c94e
devcraft1 <ukonudennis64@gmail.com>
2021-08-10 23:31:20 +0100

    correct

--- snippets/arrayMethods/mutatingArray.js
+++ snippets/arrayMethods/mutatingArray.js
@@ -1,14 +1,14 @@
-/* Mutating 
-These are array methods that change the original array they are called upon
-They include:
-1. push
-2. pop
-3. shift
-4. unshift
-5. sort
-6. reverse
-7. splice
-*/
+// /* Mutating
+// These are array methods that change the original array they are called upon
+// They include:
+// 1. push
+// 2. pop
+// 3. shift
+// 4. unshift
+// 5. sort
+// 6. reverse
+// 7. splice
+// */
 
 // PUSH
 let animals = ["dog", "cat", "tiger"];
