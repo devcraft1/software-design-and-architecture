5c4af59e33c130dea12632b31016deee9ba467cd
devcraft1 <ukonudennis64@gmail.com>
2021-08-14 23:48:14 +0100

    obj

--- snippets/prototype.js
+++ snippets/prototype.js
@@ -1,24 +1,21 @@
-
 function Person(name) {
-    this.name = name;
-  }
-  Person.prototype.getName = function() {
-    return this.name;
-  }
-  
-  var person = new Person("John Doe");
-  console.log(person.getName())
+  this.name = name;
+}
+Person.prototype.getName = function () {
+  return this.name;
+};
 
+var person = new Person("John Doe");
+console.log(person.getName());
 
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+// person
+Person.prototype.nationality = "English";
 
-  function Person(first, last, age, eye) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eye;
-    }
-    
-    Person.prototype.nationality = "English";
-    
-    var myFather = new Person("John", "Doe", 50, "blue");
-    console.log("The nationality of my father is " + myFather.nationality)
+var myFather = new Person("John", "Doe", 50, "blue");
+console.log("The nationality of my father is " + myFather.nationality);
