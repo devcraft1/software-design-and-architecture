ec069ea472aadafd6c0b76b916c7a36d21645b71
devcraft1 <ukonudennis64@gmail.com>
2021-08-14 23:44:02 +0100

    arrays

--- snippets/arrayDestructuring.js
+++ snippets/arrayDestructuring.js
@@ -1,9 +1,9 @@
-// const array = [1, 2, 7, 5, 56, 7, 8, 8];
-// const [one, two, three] = array;
-// console.log(one);
-// console.log(three);
-// console.log(array);
+const array = [1, 2, 7, 5, 56, 7, 8, 8];
+const [one, two, three] = array;
+console.log(one);
+console.log(three);
+console.log(array);
 
-// const [fisrt, second, third] = ["pop", "drink", "relax"];
+const [fisrt, second, third] = ["pop", "drink", "relax"];
 
-// console.log(fisrt);
+console.log(fisrt);
