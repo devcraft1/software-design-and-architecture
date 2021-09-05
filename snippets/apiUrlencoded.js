ec6d7cc82f9cb902edc9551814b8ef29b717ff29
devcraft1 <ukonudennis64@gmail.com>
2021-09-05 23:56:04 +0100

    api

--- snippets/apiUrlencoded.js
+++ snippets/apiUrlencoded.js
@@ -1,20 +1,23 @@
-import axios from 'axios'
-import qs from 'qs'
+import axios from 'axios';
+import qs from 'qs';
 
-function getApi(){
-    try{
-      const requestBody = {
-      key1:'value',
-      key2: 'value'
+function getApi() {
+  try {
+    const requestBody = {
+      key1: 'value',
+      key2: 'value',
+    };
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+    let res = await axios.post(url, qs.stringify(requestBody), config);
+    return res.data;
+  } catch (e) {
+    console.log(e);
   }
-  const config = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded' 
-    }}
-    let res = await axios.post(url, qs.stringify(requestBody), config)
-    return res.data
-    }catch(e){
-    console.log(e)
-    }
-  } 
-  console.log(getApi())                                                                                                                                                                                                                                                                                                                                                                                 
+}
+console.log(getApi());
+
+// api
