5b2f83105d56f62b12fdc21a43745223a48310dd
devcraft1 <ukonudennis64@gmail.com>
2021-09-16 23:39:16 +0100

    capitalize

--- snippets/capitalize.js
+++ snippets/capitalize.js
@@ -1,6 +1,10 @@
 function capitalize(str) {
   const result = str[0].toUppercase();
-  console.log(result);
+  if (str[0]) {
+    result += str[0].toUppercase();
+  } else {
+    result += str[1];
+  }
 }
 
 console.log(capitalize('string'));
