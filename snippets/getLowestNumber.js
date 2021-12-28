2d26f36ba6330a9ebc36814fc22ad4363795ffa9
devcraft1 <ukonudennis64@gmail.com>
2021-12-27 18:30:59 -0900

    lowestNumber

---
+++ snippets/getLowestNumber.js
@@ -0,0 +1,21 @@
+// create a html file and attach script to with this js file.
+
+function takeNumber(n) {
+    // alert('recent');
+    let nums = [];
+    for (let i = 0; i < n; i++) {
+        nums[i] = prompt(`Enter number ${i + 1}`)
+    }
+
+    let low = nums[0];
+
+    for (let j = 0; j < n; j++) {
+        if (nums[j] < low) {
+            low = nums[j];
+        }
+    }
+
+    alert(low);
+}
+
+takeNumber(10)
